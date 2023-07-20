b0805b47137d1ca223d79f9dd274b930ea983376
Metadev <github@infamousdegen.me>
2023-07-20 22:50:02 +0530

    updatye

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "lib/openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+contract Counter is Ownable2Step {
 
-contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
-}
+    bytes32 rootHashProof;
+    uint256 totalMawcHolder;
+    uint256 perTokenClaimable;
+    mapping(uint256 => uint256) public claimedRewardPerMawc;
+    
+    function 
